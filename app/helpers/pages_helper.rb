5d363271f885c786d437f75cbfc6649ba198fa80
Ola Tuvesson <ola@iron.clickworkorange.net>
2024-01-14 00:06:37 +0000

    Improved inline_images(). Logo swish, because why not.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,10 +2,15 @@ module PagesHelper
 
 	def inline_images(page) 
 		# TODO: handle case of too few images
-		page.body % page.images.inline.map { |image| image_to_md(page, image) }
+		page.body % page.images.inline.map { |image| image_to_md(page, image, 480) }
 	end
+	
+	#TODO: def gallery_images(), def banner_images()
 
-	def image_to_md(page, image)
-		"[![" + image.alt_text + "](" + url_for(image.image_file.variant(resize_to_limit: [480, nil])) + ")](" + page_image_path(page, image) + ")"
+	def image_to_md(page, image, width)
+		alt = image.alt_text
+		img = url_for(image.image_file.variant(resize_to_limit: [width, nil]))
+		url = page_image_path(page, image)
+		"[![#{alt}](#{img})](#{url})"
 	end
 end
