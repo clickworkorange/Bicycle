5aaec3d70649040f040910699de71fa654b34f29
Ola Tuvesson <ola@iron.clickworkorange.net>
2024-01-16 10:11:08 +0000

    menu_tree adjustments.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -17,8 +17,8 @@ module PagesHelper
 	end
 
 	def svg_icon(icon)
-		# TODO: add role="img" and aria-label="[description]" to <svg>, translating the description
-		# TODO: add aria-hidden="true" to purely decorative icons (e.g. tree-lines)
+		# TODO: add role="img" to <svg> and <title>[description]</title> inside it, translating the description
+		# TODO: add aria-hidden="true" to <svg> of purely decorative icons (e.g. tree-lines)
 		path = File.join(Rails.root, "app", "assets", "images", "icons", icon)
 		sanitize(File.read(path + ".svg"), tags: %w(svg g path), attributes: %w(xmlns width height viewBox d))
 	end
