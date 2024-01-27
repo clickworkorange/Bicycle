aee385f359904a43b0bf169ddabad04ba1484d8b
Ola Tuvesson <196348+clickworkorange@users.noreply.github.com>
2024-01-27 21:13:23 +0000

    CSS refactor. Use the "contain" property. Removed remaining "fullscreen" bits.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,21 +1,3 @@
 class ImagesController < ApplicationController
-  require "kramdown"
-  before_action :set_page
-  before_action :set_image, only: %i[show]
-
-  def show
-    return if @page.live || current_user&.admin
-
-    flash[:alert] = t("no_access")
-    redirect_to root_path
-  end
-
-  private
-  def set_page
-    @page = Page.friendly.find(params[:page_id])
-  end
-
-  def set_image
-    @image = @page.images.find(params[:id])
-  end
+ # Images are served by the front-end
 end
