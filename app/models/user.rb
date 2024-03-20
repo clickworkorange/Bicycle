c2dc2e4d579f909ee892f091631522644d838ed7
Ola Tuvesson <196348+clickworkorange@users.noreply.github.com>
2024-03-20 20:28:41 +0000

    More Devise :confirmable bits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable, :trackable and :omniauthable
-  devise :registerable, :confirmable, :validatable, :lockable, :recoverable, :rememberable, :database_authenticatable
+  devise :registerable, :confirmable, :validatable, :lockable, :recoverable, :rememberable, :reconfirmable, :database_authenticatable
   scope :admin, -> {where(admin: true)}
   validates :name, presence: true
 end
