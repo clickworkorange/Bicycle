36d244243d9689de254b5b794df7ea1416ee5626
Ola Tuvesson <196348+clickworkorange@users.noreply.github.com>
2024-04-28 15:06:49 +0100

    Added basic newsletter function.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_204352) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_28_134625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_204352) do
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
+    t.boolean "newsletter"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
