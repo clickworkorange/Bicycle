a90c4d22f79faa6232fc78e5acc91536bf54610e
Ola Tuvesson <196348+clickworkorange@users.noreply.github.com>
2024-05-14 11:19:08 +0100

    Serve "not found" image for image 404s, 404 page for HTML, PDF & ZIP and HEAD 404 for everything else. Footer is now partial.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,21 @@ class ApplicationController < ActionController::Base
   end
 
   def render_404
-    #redirect_to controller: :errors, action: :not_found
-    respond_to do |format|
-      format.html { render "errors/not_found", layout: "error", status: 404 }
-      format.all { head :not_found, content_type: "text/plain" }
+    if request.xhr?
+      head :not_found, content_type: "text/plain" 
+    else
+      respond_to do |format|
+        format.any(:html, :pdf, :zip) do
+          @page_title = t("errors.not_found.title")
+          render "errors/not_found", formats: :html, content_type: "text/html", layout: "error", status: 404 
+        end
+        format.any(:gif, :png, :jpeg) do
+          render file: "#{Rails.root}/public/not_found.png", content_type: "image/png", layout: false, status: 404
+        end
+        format.all do
+          head :not_found, content_type: "text/plain" 
+        end
+      end
     end
   end
   rescue_from ActionController::RoutingError, with: :render_404
